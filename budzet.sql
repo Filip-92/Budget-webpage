8f701dc3d02e8bf1131347abc23e85cee7dbeae5
Filip-92 <75435412+Filip-92@users.noreply.github.com>
2021-03-22 14:19:27 +0100

    Adding sql file

---
+++ budzet.sql
@@ -0,0 +1,325 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 22, 2021 at 02:18 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `budzet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expenses`
+--
+
+CREATE TABLE `expenses` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `expense_category_assigned_to_user_id` int(11) UNSIGNED NOT NULL,
+  `payment_method_assigned_to_user_id` int(11) UNSIGNED NOT NULL,
+  `amount` decimal(8,2) NOT NULL DEFAULT 0.00,
+  `date_of_expense` date NOT NULL,
+  `expense_comment` varchar(100) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expenses_category_assigned_to_users`
+--
+
+CREATE TABLE `expenses_category_assigned_to_users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expenses_category_default`
+--
+
+CREATE TABLE `expenses_category_default` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Dumping data for table `expenses_category_default`
+--
+
+INSERT INTO `expenses_category_default` (`id`, `name`) VALUES
+(1, 'Jedzenie'),
+(2, 'Mieszkanie'),
+(3, 'Transport'),
+(4, 'Telekomunikacja'),
+(5, 'Opieka zdrowotna'),
+(6, 'Ubranie'),
+(7, 'Higiena'),
+(8, 'Dzieci'),
+(9, 'Rozrywka'),
+(10, 'Wycieczka'),
+(11, 'Szkolenia'),
+(12, 'Książki'),
+(13, 'Oszczędności'),
+(14, 'Emerytura'),
+(15, 'Spłata długów'),
+(16, 'Darowizna'),
+(17, 'Inne wydatki');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incomes`
+--
+
+CREATE TABLE `incomes` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `income_category_assigned_to_user_id` int(11) UNSIGNED NOT NULL,
+  `amount` decimal(8,2) NOT NULL DEFAULT 0.00,
+  `date_of_income` date NOT NULL,
+  `income_comment` varchar(100) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incomes_category_assigned_to_users`
+--
+
+CREATE TABLE `incomes_category_assigned_to_users` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incomes_category_default`
+--
+
+CREATE TABLE `incomes_category_default` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `incomes_category_default`
+--
+
+INSERT INTO `incomes_category_default` (`id`, `name`) VALUES
+(1, 'Wynagrodzenie'),
+(2, 'Odsetki bankowe'),
+(3, 'Sprzedaż na allegro'),
+(4, 'Inne');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_methods_assigned_to_users`
+--
+
+CREATE TABLE `payment_methods_assigned_to_users` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_methods_default`
+--
+
+CREATE TABLE `payment_methods_default` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `przychody`
+--
+
+CREATE TABLE `przychody` (
+  `logged_user_id` int(11) NOT NULL,
+  `income_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `income_category` text NOT NULL,
+  `comment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `przychody`
+--
+
+INSERT INTO `przychody` (`logged_user_id`, `income_id`, `amount`, `date`, `income_category`, `comment`) VALUES
+(1, 1, 0, '2021-03-08', 'Wynagrodzenie', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uzytkownicy`
+--
+
+CREATE TABLE `uzytkownicy` (
+  `id` int(11) NOT NULL,
+  `user` text COLLATE utf8_polish_ci NOT NULL,
+  `pass` text COLLATE utf8_polish_ci NOT NULL,
+  `email` text COLLATE utf8_polish_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Dumping data for table `uzytkownicy`
+--
+
+INSERT INTO `uzytkownicy` (`id`, `user`, `pass`, `email`) VALUES
+(1, 'adam', '$2y$10$jITppKoj9RP3/L1Qlhcm.eW/avIVsY4SUdd2F5QJCYGSntvLwkjrq', 'adam@gmail.com'),
+(2, 'marek', 'asdfg', 'marek@gmail.com'),
+(3, 'anna', 'zxcvb', 'anna@gmail.com'),
+(4, 'andrzej', 'asdfg', 'andrzej@gmail.com'),
+(5, 'justyna', 'yuiop', 'justyna@gmail.com'),
+(6, 'kasia', 'hjkkl', 'kasia@gmail.com'),
+(7, 'beata', 'fgthj', 'beata@gmail.com'),
+(8, 'jakub', 'ertyu', 'jakub@gmail.com'),
+(9, 'janusz', 'cvbnm', 'janusz@gmail.com'),
+(10, 'roman', 'dfghj', 'roman@gmail.com'),
+(11, 'filip', '$2y$10$WtivvjFsiK0b7Y.bW.z7LuV88qaSNBnNEGARD1Lbvrep/d1.JbuEy', 'f_peszke@onet.pl'),
+(12, 'filip1', '$2y$10$BN5FhpL70XIqV33KddGRiewiOtKiUzHI.mgVIkPiBVc7mJYbZ2Neu', 'f_peszke@gmail.com'),
+(13, 'wojtek', '$2y$10$1BRFDfdRGIga7d4OkRqE1eGm5JgasiHf7upOFoJJuWyYSukPqPR8K', 'wojtek@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `expenses`
+--
+ALTER TABLE `expenses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `expenses_category_assigned_to_users`
+--
+ALTER TABLE `expenses_category_assigned_to_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `expenses_category_default`
+--
+ALTER TABLE `expenses_category_default`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `incomes`
+--
+ALTER TABLE `incomes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `incomes_category_assigned_to_users`
+--
+ALTER TABLE `incomes_category_assigned_to_users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `incomes_category_default`
+--
+ALTER TABLE `incomes_category_default`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payment_methods_assigned_to_users`
+--
+ALTER TABLE `payment_methods_assigned_to_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payment_methods_default`
+--
+ALTER TABLE `payment_methods_default`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `przychody`
+--
+ALTER TABLE `przychody`
+  ADD PRIMARY KEY (`logged_user_id`);
+
+--
+-- Indexes for table `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `expenses`
+--
+ALTER TABLE `expenses`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `expenses_category_assigned_to_users`
+--
+ALTER TABLE `expenses_category_assigned_to_users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `incomes`
+--
+ALTER TABLE `incomes`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `incomes_category_default`
+--
+ALTER TABLE `incomes_category_default`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `payment_methods_assigned_to_users`
+--
+ALTER TABLE `payment_methods_assigned_to_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment_methods_default`
+--
+ALTER TABLE `payment_methods_default`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
