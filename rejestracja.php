727e572612bc19c699288f7f6d89dddd5e554979
Filip-92 <75435412+Filip-92@users.noreply.github.com>
2021-03-22 14:20:56 +0100

    Registration details added

--- rejestracja.php
+++ rejestracja.php
@@ -149,54 +149,6 @@
 
 ?>
 
-<?php
-session_start();
-
-require_once 'database.php';
-
-if (!isset($_SESSION['logged_id']))
-{
-	if (isset($_POST['login']))
-	{
-		$login = filter_input(INPUT_POST, 'login');
-		$password = filter_input(INPUT_POST, 'pass');
-		
-		$userQuery = $db->prepare('SELECT id, password FROM admins WHERE login = :login');
-		$userQuery->bindValue(':login', $login, PDO::PARAM_STR);
-		$userQuery->execute();
-		
-		//echo $userQuery->rowCount();
-		
-		$user = $userQuery->fetch();
-		
-		//echo $user['id'] . " " . $user['password'];
-		
-		if ($user && password_verify($password, $user['password']))
-		{
-			$_SESSION['logged_id'] = $user['id'];
-			unset($_SESSION['bad_attampt']);
-		}
-		else
-		{
-			$_SESSION['bad_attempt'] = true;
-			header('Location: admin.php');
-			exit();
-		}
-	}
-	else
-	{
-		header('Location: admin.php');
-		exit();
-	}
-}
-
-$usersQuery = $db->query('SELECT * FROM users');
-$users = $usersQuery->fetchAll();
-
-//print_r($users);
-
-?>
-
 <!DOCTYPE HTML>
 <html lang="pl">
 <head>
